0a8939febda9b183911514d45b14edfacea3f4c9
mattsue <mattsue15@163.com>
2019-07-14 05:53:59 -0400

    添加安装说明

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 composer install
 chown -R application:application *
+php think init --db mysql://root:root@mysql:3306/sue_admin#utf8mb4 --redis @redis:6379
 php think migrate:run
 php think seed:run
